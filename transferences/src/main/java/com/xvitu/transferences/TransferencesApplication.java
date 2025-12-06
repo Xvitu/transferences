63587db221eb5ca9b0ec3d146f9e99499123a1bd
Vitor Amaral Silveira <vas17.vs@gmail.com>
2025-12-06 13:49:34 -0300

    Initial commit

---
+++ transferences/src/main/java/com/xvitu/transferences/TransferencesApplication.java
@@ -0,0 +1,13 @@
+package com.xvitu.transferences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferencesApplication.class, args);
+	}
+
+}
---
+++ transferences/src/test/java/com/xvitu/transferences/TransferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xvitu.transferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
