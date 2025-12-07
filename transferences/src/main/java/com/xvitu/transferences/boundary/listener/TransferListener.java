40d01a6736b399b7fc94b58fbcec2af478807656
Vitor Amaral Silveira <vas17.vs@gmail.com>
2025-12-07 03:57:23 -0300

    chore: add listener

--- transferences/src/main/java/com/xvitu/transferences/boundary/listener/TransferListener.java
+++ transferences/src/main/java/com/xvitu/transferences/boundary/listener/TransferListener.java
@@ -1,23 +1,37 @@
 package com.xvitu.transferences.boundary.listener;
 
+import com.xvitu.transferences.application.command.TransferCommand;
+import com.xvitu.transferences.application.usecase.TransferUseCase;
 import com.xvitu.transferences.infrastructure.rabbitmq.publisher.TransferEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Component;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class TransferListener {
+    private static final Logger logger = LoggerFactory.getLogger(TransferListener.class);
 
-    private static final long MAX_RETRIES = 3L;
+    public final TransferUseCase transferUseCase;
+
+    public TransferListener(TransferUseCase transferUseCase) {
+        this.transferUseCase = transferUseCase;
+    }
+
 
     @RabbitListener(queues = "transference.queue")
-    public void consume(TransferEvent event, @Headers Map<String, Object> headers) {
+    public void consume(TransferEvent event) {
+        logger.info("Received new message {}", event.eventId());
 
+        TransferCommand command = new TransferCommand(
+                UUID.fromString(event.eventId()), event.payerId(), event.payeeId(), event.amount()
+        );
 
-
-
+        transferUseCase.execute(command);
     }
 
 }
